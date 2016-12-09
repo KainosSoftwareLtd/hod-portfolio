1f8a3929a0833955696724563d0a8e2e73cbb269
Adrian Zabrocki <a.zabrocki@kainos.com>
2016-12-09 10:43:00 +0100

    Fix the use of ajax fail() params Remove unused code

--- app/fileStore.js
+++ app/fileStore.js
@@ -29,61 +29,6 @@ function createDirectoryIfNotExists(directory) {
 }
 
 /**
- * Get an object from S3 and save it as a file
- * 
- * @param {string} sourceObjectName Name of the object in S3 bucket - the saved file will have the same name
- * @param {string} destinationDirectory Location of the new file
- * @param {function} done Callback function
- */
-function saveBucketObject(sourceObjectName, destinationDirectory, done) {
-    var params = {
-        Key: sourceObjectName
-    };
-
-    s3.getObject(params, function(err, data) {
-        if (err) {
-            log.error(err, err.stack);
-            done();
-        } else {
-            var file = fs.createWriteStream(destinationDirectory + sourceObjectName);
-            log.debug('Downloading file: ' + sourceObjectName);
-            var stream = s3.getObject(params).createReadStream().pipe(file);
-            stream.on('finish', function() {
-                log.debug('The file has been saved to: ' + destinationDirectory + sourceObjectName);
-                done();
-            });
-        }
-    });
-};
-
-/**
- * Downloads all files from the root directory of an S3 bucket
- * WARNING: The bucket can't contain subdirectories
- * 
- * @param {string} destinationDirectory Location for the downloaded files (may not exist)
- * @param {function} done Callback 
- */
-FileStore.downloadFiles = function(destinationDirectory, done) {
-    s3.listObjects(null, function(err, data) {
-        if (err) {
-            log.error(err, err.stack);
-            done();
-        } else {
-            createDirectoryIfNotExists(destinationDirectory);
-
-            // needs to be called (data.Contents.length) times to call done()
-            var finishIfAllSaved = _.after(data.Contents.length, done);
-
-            data.Contents.forEach(function(file) {
-                saveBucketObject(file.Key, destinationDirectory, function() {
-                    finishIfAllSaved(); // will trigger done() after last object was saved
-                });
-            });
-        }
-    });
-};
-
-/**
  * Downloads all files and returns them as JSON list
  * WARNING: The bucket can't contain subdirectories
  * 
