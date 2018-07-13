91ab2715e4d2327e3f4680228c0632523280294d
marcinmo <M.Mogiela@kainos.com>
2018-07-13 11:45:42 +0200

    Removed the old codebase

--- app/assets/javascripts/application.js
+++
@@ -1,5 +0,0 @@
-// Used in tables to collapse child rows
-$('[data-collapsable-parent]').click(function(){
-	var child = $(this).attr("data-collapsable-parent");
-	$('[data-collapsable-child="' + child + '"]').toggle('fast');
-});
