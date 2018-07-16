15e9b8baed80fdb492e2a46af2f7e51479a4e29c
marcinmo <M.Mogiela@kainos.com>
2018-07-16 15:44:48 +0200

    initialize to PROJECTS

--- ui/src/app/project.service.ts
+++ ui/src/app/project.service.ts
@@ -8,32 +8,13 @@ import { PROJECTS } from './project-data';
 })
 export class ProjectService {
 
- projectArray: Project[] = [];
+  projectArray: Project[] = PROJECTS;
 
- // this doesn't work
- /*PROJECTS.foreach(function(project)
-  {
-    projectArray.push(project);
-  });*/
-
-  // this doesn't work
-  /*for(var i = 0; i < PROJECTS.length; i++)
-  {
-    this.projectArray.Add(PROJECTS[i]);
-  }*/
-
-    constructor() { }
+  constructor() { }
 
   getProjects(): Project[] {
     return this.projectArray;
   }
-
-  // should I be calling this from component to populate? and if so whats the point in a service because then
-  // I'm still accessing PROJECTS from component instead of seperating everything out
-  /*addProjects(project)
-  {
-    this.projectsArray.push(project);
-  }*/
 
   addProject(project: Project) {
     this.projectArray.push(project);
