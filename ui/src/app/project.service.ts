c94368d012a0a74f4476631ce6f56d0d6771069c
Phoebe McBride <p.mcbride@kainos.com>
2018-07-17 13:14:48 +0100

    imported aws s3 sdk and updated service to access s3 bucket

--- ui/src/app/project.service.ts
+++ ui/src/app/project.service.ts
@@ -2,17 +2,51 @@ import { Injectable } from '@angular/core';
 
 import { Project } from './project';
 import { PROJECTS } from './project-data';
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
+
+const bucket = new S3(
+  {
+    accessKeyId: 'AKIAI5GLSVP7MIIJMFFQ',
+    secretAccessKey: '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',
+    region: 'eu-west-1'
+  }
+);
+ 
+const params = {
+  Bucket: 'gov-project-dashboard-dev',
+  Key: 'gov-project-dashboard-dev/',
+};
+ 
+this.getS3Bucket().listObjects(params, function (err, data) {
+  if (err) {
+    console.log('There was an error getting your files: ' + err);
+    return;
+  }
+
+  data.contents;
+});
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
+  
+
   projectArray: Project[] = PROJECTS;
 
   constructor() { }
 
   getProjects(): Project[] {
+    this.getS3Bucket().listObjects(params, function (err, data) {
+      if (err) {
+        console.log('There was an error getting your files: ' + err);
+        return;
+      }
+    
+      data.contents;
+    });
     return this.projectArray;
   }
 
